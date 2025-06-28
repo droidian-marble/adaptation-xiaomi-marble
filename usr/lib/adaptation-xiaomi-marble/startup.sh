3fde004967feed6efa832838be304c8c613901e7
Halhadus <eneshakans45@proton.me>
2025-06-28 08:30:58 +0300

    mount-android.sh scripts updated

--- usr/lib/adaptation-xiaomi-marble/startup.sh
+++ usr/lib/adaptation-xiaomi-marble/startup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 if [ -f /userdata/.dualboot ]; then
-    mount --bind /usr/lib/adaptation-xiaomi-marble/mount-android.sh.dualboot /usr/sbin/mount-android.sh
+    if [ -f /userdata/super.img ]; then
+        mount --bind /usr/lib/adaptation-xiaomi-marble/mount-android.sh.superimg /usr/sbin/mount-android.sh
+    else
+        mount --bind /usr/lib/adaptation-xiaomi-marble/mount-android.sh.dualboot /usr/sbin/mount-android.sh
+    fi
 else
     mount --bind /usr/lib/adaptation-xiaomi-marble/mount-android.sh /usr/sbin/mount-android.sh
 fi
