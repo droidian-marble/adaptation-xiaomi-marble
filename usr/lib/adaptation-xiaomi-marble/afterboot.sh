ccb526c843dfecdcfdc6431606ece4a93103f87f
Halhadus <eneshakans45@proton.me>
2026-09-10 16:50:44 +0300

    Not from zero but somethings are changed

--- usr/lib/adaptation-xiaomi-marble/afterboot.sh
+++ usr/lib/adaptation-xiaomi-marble/afterboot.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 sleep 3
 systemctl restart ofono ModemManager
-if [ -e "/usr/lib/systemd/system/waydroid-container.service" ]; then
-    systemctl restart waydroid-container
-fi
 systemctl restart usb-tethering
